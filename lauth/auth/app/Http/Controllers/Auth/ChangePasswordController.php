009eb4a86812c3629cb3fb04e0ca447be14bef79
Wididou <fw_dekkiche@esi.dz>
2020-03-11 11:30:24 +0100

    add change password & messages

---
+++ lauth/auth/app/Http/Controllers/Auth/ChangePasswordController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+use App\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class ChangePasswordController extends Controller
+{
+    public function index()
+    {
+        return view('auth.passwords.change');
+    }
+    public function changepwd(Request $request)
+    {
+        $this->validate($request,[
+            'oldpassword' => 'required',
+            'password' => 'required|confirmed'
+        ]);
+        $hashpassword = Auth::user()->password;
+        if (Hash::check($request->oldpassword,$hashpassword)) {
+            $user = User::find(Auth::id());
+            $user->password = Hash::make($request->password);
+            $user->save();
+            Auth::logout();
+            return redirect()->route('login')->with('successMsg', 'Password successfully changed');
+        }else{
+            return redirect()->back()->with('errorMsg', 'Current Password is invalid');
+        }
+    }
+}
